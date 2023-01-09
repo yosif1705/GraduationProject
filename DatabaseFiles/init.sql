b08378d9f1c5f5d4a9d55ddebe6092f17622d418
Yosif Taibi <yosif.taibi@gmail.com>
2023-01-09 02:44:39 +0200

    Added constraints after table creations

--- DatabaseFiles/init.sql
+++ DatabaseFiles/init.sql
@@ -10,9 +10,7 @@ CREATE TABLE users (
     password_hash TEXT NOT NULL,
     `name` varchar(255),
     `status` ENUM('ONLINE', 'OFFLINE', "LISTENING") NOT NULL DEFAULT 'OFFLINE',
-    playing_song_id INTEGER DEFAULT NULL,
-    
-	FOREIGN KEY(playing_song_id) REFERENCES songs (song_id) ON DELETE SET NULL
+    playing_song_id INTEGER DEFAULT NULL
 );
 
 CREATE TABLE songs (
@@ -22,49 +20,51 @@ CREATE TABLE songs (
     title VARCHAR(255) NOT NULL,
     duration INTEGER NOT NULL,
     `path` VARCHAR(255) NOT NULL,
-    is_local BOOLEAN NOT NULL,
-    
-    FOREIGN KEY(uploader_id) REFERENCES users (user_id) ON DELETE CASCADE
+    is_local BOOLEAN NOT NULL
 );
 
 CREATE TABLE playlists (
     playlist_id INTEGER PRIMARY KEY AUTO_INCREMENT,
     
     user_id INTEGER NOT NULL,
-    `name` VARCHAR(255) NOT NULL,
-    
-    FOREIGN KEY (user_id) REFERENCES users (user_id) ON DELETE CASCADE
+    `name` VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE playlist_songs (
     playlist_id INTEGER NOT NULL,
     song_id INTEGER NOT NULL,
-    
     playlist_index INTEGER NOT NULL,
     
-    PRIMARY KEY (playlist_id, song_id),
-    FOREIGN KEY (playlist_id) REFERENCES playlists (playlist_id) ON DELETE CASCADE,
-    FOREIGN KEY (song_id) REFERENCES songs (song_id) ON DELETE CASCADE
+    PRIMARY KEY (playlist_id, song_id)
 );
 
 CREATE TABLE users_relations (
     user_id_1 INTEGER NOT NULL,
     user_id_2 INTEGER NOT NULL,
-    
     friendship_status ENUM('REQUESTED', 'ACCEPTED', 'BLOCKED') NOT NULL,
     
-    PRIMARY KEY (user_id_1, user_id_2, friendship_status),
-    FOREIGN KEY (user_id_1) REFERENCES users (user_id) ON DELETE CASCADE,
-    FOREIGN KEY (user_id_2) REFERENCES users (user_id) ON DELETE CASCADE
+    PRIMARY KEY (user_id_1, user_id_2, friendship_status)
 );
 
 CREATE TABLE songs_discoveries (
     user_id INTEGER NOT NULL,
     song_id INTEGER NOT NULL,
-    
     discovery_date DATE NOT NULL,
 
-	PRIMARY KEY (user_id, song_id, discovery_date),	
-    FOREIGN KEY (user_id) REFERENCES users (user_id) ON DELETE CASCADE,
-    FOREIGN KEY (song_id) REFERENCES songs (song_id) ON DELETE CASCADE
+	PRIMARY KEY (user_id, song_id, discovery_date)
 );
+
+ALTER TABLE users ADD FOREIGN KEY(playing_song_id) REFERENCES songs (song_id) ON DELETE SET NULL;
+
+ALTER TABLE songs ADD FOREIGN KEY(uploader_id) REFERENCES users (user_id) ON DELETE CASCADE;
+
+ALTER TABLE playlists ADD FOREIGN KEY (user_id) REFERENCES users (user_id) ON DELETE CASCADE;
+
+ALTER TABLE playlist_songs ADD FOREIGN KEY (playlist_id) REFERENCES playlists (playlist_id) ON DELETE CASCADE, 
+			   ADD FOREIGN KEY (song_id) REFERENCES songs (song_id) ON DELETE CASCADE;
+
+ALTER TABLE users_relations ADD FOREIGN KEY (user_id_1) REFERENCES users (user_id) ON DELETE CASCADE,
+			    ADD FOREIGN KEY (user_id_2) REFERENCES users (user_id) ON DELETE CASCADE;
+
+ALTER TABLE songs_discoveries ADD FOREIGN KEY (user_id) REFERENCES users (user_id) ON DELETE CASCADE,
+			      ADD FOREIGN KEY (song_id) REFERENCES songs (song_id) ON DELETE CASCADE;
