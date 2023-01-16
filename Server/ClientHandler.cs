826dde7bddf9a3a0b91d100368301c75421aa96c
Yosif Taibi <yosif.taibi@gmail.com>
2023-01-16 03:48:56 +0200

    Server files

---
+++ Server/ClientHandler.cs
@@ -0,0 +1,54 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading;
+
+class ClientHandler
+{
+    public event EventHandler<CommandReceivedEventArgs> CommandReceived;
+
+    private TcpClient _client;
+    private bool _isRunning;
+    private Thread _receiveThread;
+
+    public ClientHandler(TcpClient client)
+    {
+        _client = client;
+        _receiveThread = new Thread(ReceiveCommands);
+    }
+
+    public void Start()
+    {
+        _isRunning = true;
+        _receiveThread.Start();
+    }
+
+    public void Stop()
+    {
+        _isRunning = false;
+        _client.Close();
+    }
+
+    private void ReceiveCommands()
+    {
+        while (_isRunning)
+        {
+            try
+            {
+                var buffer = new byte[1024];
+                var bytesReceived = _client.GetStream().Read(buffer, 0, buffer.Length);
+                var command = Encoding.ASCII.GetString(buffer, 0, bytesReceived);
+                RaiseCommandReceivedEvent(command, _client.Client.RemoteEndPoint);
+            }
+            catch (Exception)
+            {
+                Stop();
+            }
+        }
+    }
+
+    private void RaiseCommandReceivedEvent(string command, EndPoint clientAddress)
+    {
+        CommandReceived?.Invoke(this, new CommandReceivedEventArgs(command, clientAddress));
+    }
+}
---
+++ Server/Database.cs
@@ -0,0 +1,104 @@
+﻿using MySqlConnector;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    public class Database
+    {
+        private MySqlConnection _connection;
+        
+        public Database(string server, string databaseName, string userName, string password)
+        {
+            MySqlConnectionStringBuilder connectionStringBuilder = new MySqlConnectionStringBuilder
+            {
+                { "Server", server },
+                { "Database", databaseName },
+                { "UID", userName },
+                { "password", password }
+            };
+            _connection = new MySqlConnection(connectionStringBuilder.ConnectionString);
+        }
+
+        public void Open()
+        {
+               
+            _connection.Open();
+            
+        }
+
+        public void Close()
+        {
+            _connection.Close();
+        }
+
+        public async Task<List<Dictionary<string, object>>> ExecuteQuery(string query, Dictionary<string, object> parameters)
+        {
+            using (var command = new MySqlCommand(query, _connection))
+            {
+                foreach (var parameter in parameters)
+                {
+                    command.Parameters.AddWithValue(parameter.Key, parameter.Value);
+                }
+
+                using (var reader = await command.ExecuteReaderAsync())
+                {
+                    var result = new List<Dictionary<string, object>>();
+                    while (await reader.ReadAsync())
+                    {
+                        var row = new Dictionary<string, object>();
+                        for (int i = 0; i < reader.FieldCount; i++)
+                        {
+                            row.Add(reader.GetName(i), reader.GetValue(i));
+                        }
+                        result.Add(row);
+                    }
+                    return result;
+                }
+            }
+        }
+        public async Task<int> ExecuteNonQuery(string query, Dictionary<string, object> parameters)
+        {
+            using (var command = new MySqlCommand(query, _connection))
+            {
+                foreach (var parameter in parameters)
+                {
+                    command.Parameters.AddWithValue(parameter.Key, parameter.Value);
+                }
+
+                return await command.ExecuteNonQueryAsync();
+            }
+        }
+        public async Task<object> ExecuteScalar(string query, Dictionary<string, object> parameters = null)
+        {
+            using (var command = new MySqlCommand(query, _connection))
+            {
+                if (parameters != null)
+                {
+                    command.Parameters.AddRange(parameters.Select(p => new MySqlParameter(p.Key, p.Value)).ToArray());
+                }
+
+                return await command.ExecuteScalarAsync();
+            }
+        }
+        public async Task<DataTable> ExecuteDataTable(string query, Dictionary<string, object> parameters = null)
+        {
+            using (var command = new MySqlCommand(query, _connection))
+            {
+                if (parameters != null)
+                {
+                    command.Parameters.AddRange(parameters.Select(p => new MySqlParameter(p.Key, p.Value)).ToArray());
+                }
+
+                var dataTable = new DataTable();
+                dataTable.Load(await command.ExecuteReaderAsync());
+                return dataTable;
+            }
+        }
+    }
+
+ }
